6d9a49aeaf0328344efccc34cef213f31f1c7c95
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2024-12-07 12:39:49 -0500

    defined ratings sample data in catalog service and established the communication with movie info service using RestTemplate

--- movie-catalog-service/src/main/java/io/javabrains/movie_catalog_service/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/movie_catalog_service/resources/MovieCatalogResource.java
@@ -2,11 +2,16 @@ package io.javabrains.movie_catalog_service.resources;
 
 
 import io.javabrains.movie_catalog_service.models.CatalogItem;
+import io.javabrains.movie_catalog_service.models.Movie;
+import io.javabrains.movie_catalog_service.models.Rating;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -15,10 +20,26 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(String userId){
 
-        return Collections.singletonList(
-                new CatalogItem("shutter Island", "test", 4)
+        // get all rated movie IDs
+        RestTemplate restTemplate = new RestTemplate();
 
-            );
+
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
+        );
+
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+                })
+                .collect(Collectors.toList());
+
+        //For each movie ID, call movie info service and get details
+
+        //Put them all together
+
     }
 
 }
