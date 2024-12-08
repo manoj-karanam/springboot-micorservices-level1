899bf0b733d2a9e03b9fde1d3fad384ef4d4b713
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2024-12-07 19:37:06 -0500

    built microservices with two api calls synchronously and also returned userRating object(returned an object instead of a list)

--- movie-catalog-service/src/main/java/io/javabrains/movie_catalog_service/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/movie_catalog_service/resources/MovieCatalogResource.java
@@ -4,10 +4,13 @@ package io.javabrains.movie_catalog_service.resources;
 import io.javabrains.movie_catalog_service.models.CatalogItem;
 import io.javabrains.movie_catalog_service.models.Movie;
 import io.javabrains.movie_catalog_service.models.Rating;
+import io.javabrains.movie_catalog_service.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -22,26 +25,37 @@ public class MovieCatalogResource {
     @Autowired //consumer - it checks for objects if its already defined and uses that instance
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
-    public List<CatalogItem> getCatalog(String userId){
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
         // get all rated movie IDs
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("5678", 3)
-        );
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
 
-        return ratings.stream().map(rating -> {
+        return ratings.getUserRating().stream().map(rating -> {
+            //using Synchronous approach using RestTemplate
+            // For each movie ID, call movie info service and get details
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+
             return new CatalogItem(movie.getName(), "Desc", rating.getRating());
                 })
                 .collect(Collectors.toList());
-
-        //For each movie ID, call movie info service and get details
-
         //Put them all together
-
     }
 
 }
+
+
+//using Asynchronous approach using WebClient
+            /*
+            Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/"+rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+
+             */
--- ratings-data-service/src/main/java/io/javabrains/ratings_data_service/resources/RatingsDataResource.java
+++ ratings-data-service/src/main/java/io/javabrains/ratings_data_service/resources/RatingsDataResource.java
@@ -1,20 +1,36 @@
 package io.javabrains.ratings_data_service.resources;
 
 import io.javabrains.ratings_data_service.models.Rating;
+import io.javabrains.ratings_data_service.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsDataResource {
 
     @RequestMapping("/{movieId}")
-    public static Rating getRating(@PathVariable("movieId") String movieId){
+    public Rating getRating(@PathVariable("movieId") String movieId){
 
         return(
                 new Rating(movieId, 4)
                 );
-
     }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
+    }
+
+
 }
